0df8b8a8e1e4ffbef076e35480b4d7b8a484300d
sanderdebr <sander606@gmail.com>
2020-03-12 17:19:21 +0100

    'second'

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+    <employee-form @add:employee="addEmployee" />
+    <employee-table :employees="employees" />
+  </div>
+</template>
+
+<script>
+import EmployeeTable from "@/components/EmployeeTable.vue";
+import EmployeeForm from "@/components/EmployeeForm.vue";
+
+export default {
+  name: "App",
+  components: {
+    EmployeeTable,
+    EmployeeForm
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: "Richard Hendricks",
+          email: "richard@piedpiper.com"
+        },
+        {
+          id: 2,
+          name: "Bertram Gilfoyle",
+          email: "gilfoyle@piedpiper.com"
+        },
+        {
+          id: 3,
+          name: "Dinesh Chugtai",
+          email: "dinesh@piedpiper.com"
+        }
+      ]
+    };
+  },
+  methods: {
+    addEmployee(employee) {
+      const id = this.employees.length + 1;
+      const newEmployee = { ...employee, id };
+      this.employees = [...this.employees, newEmployee];
+    }
+  }
+};
+</script>
+
+<style>
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
+.small-container {
+  max-width: 680px;
+}
+</style>
